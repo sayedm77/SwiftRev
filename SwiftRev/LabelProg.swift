49fd91385c52f32eaa85009b98912d34735470fb
Elsayed Mansour <sayedmansoo123@gmail.com>
2024-12-09 09:54:03 +0200

    create button style programatically

--- SwiftRev/LabelProg.swift
+++ SwiftRev/LabelProg.swift
@@ -13,26 +13,47 @@ class LabelProg: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        lblCreate()
+//        lblCreate()
+        buttonCreate()
+        
     }
     
-    func lblCreate() {
-        // basic Setup
-        let label = UILabel()
-        label.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
-        label.text = "Hello Swift \n Hello Developer"
-        label.numberOfLines = 2
-        self.view.addSubview(label)
-        
-        //Allignment
-        label.center = self.view.center
-        label.textAlignment = .center
-        
-        // background
-        label.backgroundColor = UIColor.red
-        label.textColor = UIColor.white
+//    func lblCreate() {
+//        // basic Setup
+//        let label = UILabel()
+//        label.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
+//        label.text = "Hello Swift \n Hello Developer"
+//        label.numberOfLines = 2
+//        self.view.addSubview(label)
+//        
+//        //Allignment
+//        label.center = self.view.center
+//        label.textAlignment = .center
+//        
+//        // background
+//        label.backgroundColor = UIColor.red
+//        label.textColor = UIColor.white
+//        
+//    }
+
+    func buttonCreate() {
+        // set UP
+        let button = UIButton()
+        button.frame = CGRect(x: 90, y: 600, width: 200, height: 50)
+        button.setTitle("Save", for: .normal)
+        self.view.addSubview(button)
+        //background
+        button.backgroundColor = .green
+        button.layer.cornerRadius = 25
+        //action
+        button.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
         
     }
 
-
+    @objc func saveButtonTapped() {
+        self.view.backgroundColor = .blue
+        print("welcome To X-Code")
+        
+    }
+    
 }
