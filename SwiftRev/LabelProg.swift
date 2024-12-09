b8e139be034f2cef8d614d2103736ebacf964553
Elsayed Mansour <sayedmansoo123@gmail.com>
2024-12-09 10:45:24 +0200

    Create both simple and action

--- SwiftRev/LabelProg.swift
+++ SwiftRev/LabelProg.swift
@@ -40,10 +40,11 @@ class LabelProg: UIViewController {
         // set UP
         let button = UIButton()
         button.frame = CGRect(x: 90, y: 600, width: 200, height: 50)
-        button.setTitle("Save", for: .normal)
+        button.setTitle("show alert ", for: .normal)
         self.view.addSubview(button)
+        button.center = self.view.center
         //background
-        button.backgroundColor = .green
+        button.backgroundColor = .systemBlue
         button.layer.cornerRadius = 25
         //action
         button.addTarget(self, action: #selector(saveButtonTapped), for: .touchUpInside)
@@ -51,8 +52,42 @@ class LabelProg: UIViewController {
     }
 
     @objc func saveButtonTapped() {
-        self.view.backgroundColor = .blue
-        print("welcome To X-Code")
+//        self.view.backgroundColor = .blue
+//        print("welcome To X-Code")
+//        createSimpleAlert()
+        createActionSheetAlert()
+        
+    }
+    
+    // MARK: -   creete alert (simple , action Sheet)
+    
+   
+    
+    func createSimpleAlert() {
+        let alert = UIAlertController(title: "Heeey", message: "do you want to change the background", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: { action in
+            print("No Button Tapped")
+            self.view.backgroundColor = .systemBackground
+        }))
+        alert.addAction(UIAlertAction(title: "yes", style: .default, handler: { action in
+            print("yes,Button Tapped")
+            self.view.backgroundColor = .gray
+        }))
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func createActionSheetAlert() {
+        let alert = UIAlertController(title: "Heeey", message: "do you want to change the background", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "No", style: .default, handler: { action in
+            print("No Button Tapped")
+            self.view.backgroundColor = .systemBackground
+        }))
+        alert.addAction(UIAlertAction(title: "yes", style: .default, handler: { action in
+            print("yes,Button Tapped")
+            self.view.backgroundColor = .darkGray
+        }))
+        present(alert, animated: true, completion: nil)
         
     }
     
