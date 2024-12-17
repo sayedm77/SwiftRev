14343e3181094ba6182660ffe3b7477af6a2fc07
Elsayed Mansour <sayedmansoo123@gmail.com>
2024-12-17 16:03:41 +0200

    navigate using both sroryboard and segue

---
+++ SwiftRev/SecViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecViewController.swift
+//  SwiftRev
+//
+//  Created by sayed mansour on 17/12/2024.
+//
+
+import UIKit
+
+class SecViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
